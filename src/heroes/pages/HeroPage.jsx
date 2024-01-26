0f441eb9a8d1cecf6760b2976aa73f53172de857
Dayana Castro <dayanacastro@Dayanas-MacBook-Air.local>
2024-01-26 10:31:25 -0600

    docs creado

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -34,9 +34,8 @@ export const HeroPage = () => {
      
     <img 
     // src={` /assets/heroes/${id}.jpg`}
-    src={`../heroes/${id}.jpg`}
+    src={`/heroes/${ id }.jpg`}
 
-  
     alt={ hero.superhero }
     className="img-thumbnail animate__animated animate__fadeInLeft"
     />
